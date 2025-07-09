5dea873ed2479ff9d2b6bab9b6d949d1be23ba1a
Yeoh, Hoong Tee <hoong.tee.yeoh@intel.com>
2025-07-09 07:46:26 +0530

    hmi-augmented-worker: Update setup script placeholder
    Signed-off-by: Yeoh, Hoong Tee <hoong.tee.yeoh@intel.com>

--- manufacturing-ai-suite/hmi-augmented-worker/file_watcher/set_env_vars.bat
+++ manufacturing-ai-suite/hmi-augmented-worker/file_watcher/set_env_vars.bat
@@ -1,4 +1,4 @@
 @echo off
-set WATCH_DIRECTORY=<your_file_directory>
-set DOCUMENT_ENDPOINT=http://<your_backend_service_ip>:8888/documents
-set no_proxy=<your_backend_service_ip>
+set WATCH_DIRECTORY=<your-file-directory>
+set DOCUMENT_ENDPOINT=http://<your-backend-service-ip>:8888/documents
+set no_proxy=<your-backend-service-ip>
